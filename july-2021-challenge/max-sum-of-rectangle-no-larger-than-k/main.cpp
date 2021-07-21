22026c7055a72a01d672f72cdd28b04290787e1d
bayda <vladimir.bayda@gmail.com>
2021-07-21 00:42:43 -0400

    problem: container with most water

--- july-2021-challenge/max-sum-of-rectangle-no-larger-than-k/main.cpp
+++ july-2021-challenge/max-sum-of-rectangle-no-larger-than-k/main.cpp
@@ -1,24 +1,20 @@
 #include <vector>
 
 /**
- * Given an m x n matrix matrix and an integer k, return the max sum of a rectangle
+ * Given an m x n matrix and an integer k, return the max sum of a rectangle
  * in the matrix such that its sum is no larger than k.
  *
  * It is guaranteed that there will be a rectangle with a sum no larger than k.
  *
- *
- *
  * Example 1:
- *
- *
  * Input: matrix = [[1,0,1],[0,-2,3]], k = 2
  * Output: 2
  * Explanation: Because the sum of the blue rectangle [[0, 1], [-2, 3]] is 2, and
- * 2 is the max number no larger than k (k = 2). Example 2:
+ * 2 is the max number no larger than k (k = 2).
  *
+ * Example 2:
  * Input: matrix = [[2,2,-1]], k = 3
  * Output: 3
- *
  *
  * Constraints:
  *
@@ -28,17 +24,85 @@
  * -100 <= matrix[i][j] <= 100
  * -105 <= k <= 105
  *
- *
  * Follow up: What if the number of rows is much larger than the number of columns?
  */
-
 class Solution
 {
 public:
-    int maxSumSubmatrix(std::vector<std::vector<int>>& matrix, int k) {}
+    int result = INT_MIN;
+    void updateResult(vector<int>& nums, int k)
+    {
+        int sum = 0;
+
+        // Container to store sorted prefix sums.
+        set<int> sortedSum;
+        set<int>::iterator it;
+
+        // Add 0 as the prefix sum for an empty sub-array.
+        sortedSum.insert(0);
+        for (int& num : nums)
+        {
+            // Running Sum.
+            sum += num;
+
+            // Get X where Running sum - X <= k such that sum - X is closest to k.
+            it = sortedSum.lower_bound(sum - k);
+
+            // If such X is found in the prefix sums.
+            // Get the sum of that sub array and update the global maximum resul.
+            if (it != sortedSum.end())
+                result = max(result, sum - *it);
+
+            // Insert the current running sum to the prefix sums container.
+            sortedSum.insert(sum);
+        }
+    }
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k)
+    {
+        // Stores the 1D representation of the matrix.
+        vector<int> rowSum(matrix[0].size());
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            // Initialize the 1D representation with 0s.
+            fill(rowSum.begin(), rowSum.end(), 0);
+
+            // We convert the matrix between rows i..row inclusive to 1D array
+            for (int row = i; row < matrix.size(); row++)
+            {
+                // Add the current row to the previous row.
+                // This converts the matrix between i..row to 1D array
+                for (int col = 0; col < matrix[0].size(); col++)
+                    rowSum[col] += matrix[row][col];
+
+                // Run the 1D algorithm for `rowSum`
+                updateResult(rowSum, k);
+
+                // If result is k, this is the best possible answer, so return.
+                if (result == k)
+                    return result;
+            }
+        }
+        return result;
+    }
+};
+class Solution
+{
+public:
+    using Row    = std::vector<int>;
+    using Matrix = std::vector<Row>;
+    int maxSumSubmatrix(Matrix& matrix, int k)
+    {
+        return 42;
+    }
 };
 
 namespace
 {
-TEST(SolutionTests, All) {}
+TEST(SolutionTests, All)
+{
+    Solution::Matrix matrix{
+        {1, 0, 1},  //
+        {0, -2, 3}  //
+    };
+}
 }  // namespace
