89ff9cb4b4ce742ccfacfc8a7524de04a8e460e0
bayda <vladimir.bayda@gmail.com>
2021-08-21 22:40:24 -0400

    problem: find-greatest-common-divisor-of-array

---
+++ weekly-contest-255/5850-find-greatest-common-divisor-of-array/main.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+/**
+ * url: https://leetcode.com/contest/weekly-contest-255/problems/find-greatest-common-divisor-of-array/
+ * problem:
+ * Given an integer array nums, return the greatest common divisor of the
+ * smallest number and largest number in nums.
+ *
+ * The greatest common divisor of two numbers is the largest positive integer
+ * that evenly divides both numbers.
+ *
+ *
+ *
+ * Example 1:
+ * Input: nums = [2,5,6,9,10]
+ * Output: 2
+ * Explanation:
+ * The smallest number in nums is 2.
+ * The largest number in nums is 10.
+ * The greatest common divisor of 2 and 10 is 2.
+ *
+ * Example 2:
+ * Input: nums = [7,5,6,8,3]
+ * Output: 1
+ * Explanation:
+ * The smallest number in nums is 3.
+ * The largest number in nums is 8.
+ * The greatest common divisor of 3 and 8 is 1.
+ *
+ * Example 3:
+ * Input: nums = [3,3]
+ * Output: 3
+ * Explanation:
+ * The smallest number in nums is 3.
+ * The largest number in nums is 3.
+ * The greatest common divisor of 3 and 3 is 3.
+ *
+ *
+ * Constraints:
+ *
+ * 2 <= nums.length <= 1000
+ * 1 <= nums[i] <= 1000
+ */
+
+class Solution
+{
+public:
+    int findGCD(std::vector<int> const& nums)
+    {
+        auto min = *std::min_element(std::begin(nums), std::end(nums));
+        auto max = *std::max_element(std::begin(nums), std::end(nums));
+        for (int i = min; i >= 2; --i)
+        {
+            if (min % i == 0 && max % i == 0)
+                return i;
+        }
+        return 1;
+    }
+};
+namespace
+{
+TEST(SolutionTests, All)
+{
+    EXPECT_EQ(2, Solution{}.findGCD({2, 5, 6, 9, 10}));
+    EXPECT_EQ(1, Solution{}.findGCD({7, 5, 6, 8, 3}));
+    EXPECT_EQ(3, Solution{}.findGCD({3, 3}));
+}
+}  // unnamed namespace
